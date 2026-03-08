448f76d557c58f3a4de63965206cb0221ed4481b
SOTOKATA <6vovka213@gmail.com>
2026-03-08 21:11:57 +0000

    Update 00.55.88: Remove unused Project
    - Remove project "NumericLib"

--- NumericLib/NumericNamespace.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.NativeLib;
-
-namespace NumericLib;
-
-public class NumericNamespace : IQlangNamespace
-{
-    public string Name { get; set; } = "numeric";
-    public List<IQlangClass> Classes { get; set; } = [new NumericOperationsClass()];
-}
